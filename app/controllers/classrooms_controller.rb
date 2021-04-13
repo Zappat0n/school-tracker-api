28e374008bd580e6b67b410e709abbe5d7d8c95b
Zappat0n <albarros1973@gmail.com>
2021-04-13 13:26:55 +0200

    Add Doorkeeper configuration

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,5 +1,5 @@
 class ClassroomsController < ApplicationController
-  before_action :set_classroom, only: [:show, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /classrooms
   def index
@@ -39,13 +39,14 @@ class ClassroomsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_classroom
-      @classroom = Classroom.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def classroom_params
-      params.require(:classroom).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_classroom
+    @classroom = Classroom.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def classroom_params
+    params.require(:classroom).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
   devise_for :users
   root 'classrooms#index'
   resources :classrooms
