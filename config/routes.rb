7bd14581aeb5957cf83538fe64bb34219da0a412
Zappat0n <albarros1973@gmail.com>
2021-04-15 13:21:07 +0200

    Add students#index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   end
 
   post '/users', to: 'users#create'
-
   resources :classrooms
+
+  get '/students', to: 'students#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
