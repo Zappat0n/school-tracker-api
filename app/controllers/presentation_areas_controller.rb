6391b0718631c8b15d0551eec603f8c8521dde4a
Zappat0n <albarros1973@gmail.com>
2021-04-16 16:40:45 +0200

    Add PresentationAreas index and show methods

--- app/controllers/presentation_areas_controller.rb
+++ app/controllers/presentation_areas_controller.rb
@@ -1,2 +1,14 @@
 class PresentationAreasController < ApplicationController
+  # GET /presentation_areas
+  def index
+    @presentation_areas = PresentationArea.all
+    render json: @presentation_areas
+  end
+
+  # GET /presentation_areas/1
+  def show
+    @presentation_area = PresentationArea.find(params[:id])
+    @presentation_subareas = PresentationSubarea.where(presentation_area: @presentation_area)
+    render json: @presentation_subareas
+  end
 end
